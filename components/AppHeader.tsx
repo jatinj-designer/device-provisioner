7f6172caa018ddaf678852e489ee0e42dde7824e
jatinjindalJJ <reachtojatin1@gmail.com>
2026-06-18 16:01:44 +0530

    Remove Install app button from header

--- components/AppHeader.tsx
+++ components/AppHeader.tsx
@@ -1,13 +1,10 @@
 'use client';
 
-import InstallButton from './InstallButton';
-
 export default function AppHeader() {
   return (
     <header className="app">
       <h1>Device Provisioner</h1>
       <span className="tag">8-byte payload</span>
-      <InstallButton />
       <p>
         Choose a hardware version, give it a manufacturing number, mint a unique device ID, and emit the binary your
         bootloader reads.{' '}
